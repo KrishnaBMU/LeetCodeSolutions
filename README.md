4220aebdb6b3c4e5f95148844e41a04f9006f07e
Sheeri <sheeri@hey.com>
2021-10-15 13:54:47 -0400

    Actually write to the README file.

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-# title: README
-# description: Here are the files in the repo, with a title and description.
-# description: generated from generate_readme.py
+# LeetCode Solutions
+This repository has unofficial solutions to the challenges at https://leetcode.com/problemset/all/
 
++-- star.cpp
++-- Length of Last Word
+Python/
+|  +-- easy/
+|  |  +-- Valid Perfect Square
+|  |  +-- Search in rotated sorted array
+|  |  +-- IsOneBitCharacter.py
+|  |  +-- Length of Last Word
+|  |  +-- addonerow.py
+|  +-- hard/
+|  |  +-- sudokuSolver.py
+|  |  +-- MedianOfArrays.py
+|  +-- medium/
+|  |  +-- PerfectSquares.py
+|  |  +-- reverse_int.py
+Java/
+|  +-- Medium/
+|  |  +-- Search2DMatrix.java
--- generateREADME.py
+++ generateREADME.py
@@ -4,15 +4,14 @@ import re
 
 # title: Generate a README file for this repo
 # description: Looks through all the files and writes a markdown file to README.md
-# description: has a list of exclude file patterns (e.g. *.swp) and directories (e.g. .git, configs)
+# description: has a list of exclude file patterns (e.g. *.swp) and directories (e.g. .git, readme files, this file)
 
 path = "./"
-filelist = []
-contents = ""
-excludefiles = ['.*\.swp', 'CONTRIBUTING.md', 'README.md']
+contents="# LeetCode Solutions\nThis repository has unofficial solutions to the challenges at https://leetcode.com/problemset/all/\n"
+
+excludefiles = ['.*\.swp', os.path.basename(__file__),'CONTRIBUTING.md', 'README.md']
 # excluded directories should be comma-separated and have the format '^%s{DIR}' % path
 excludedir = ['^%s.git' % path, '^%sconfigs' % path]
-
 for root, dirs, files in os.walk(path):
     directory_level = root.replace(path, "")
     directory_level = directory_level.count(os.sep)
@@ -27,47 +26,17 @@ for root, dirs, files in os.walk(path):
         prefix="|  "
 
       if (root != './'): 
-        print("{}{}{}/".format(prefix*directory_level, indent*(directory_level), os.path.basename(root)))
-#      contents+=print("{}{}{}/".format(prefix*directory_level, indent*(directory_level), os.path.basename(root)))
-#     filelist.append(os.path.join(root,file))
+        contents+=("\n{}{}{}/".format(prefix*directory_level, indent*(directory_level), os.path.basename(root)))
       for file in files:
         testme = '(?:% s)' % '|'.join(excludefiles)
         if (re.search(testme,file)): next
-#        else: filelist.append(os.path.join(root,file))
-        else: print("{}{}{}".format(prefix*(directory_level+1), indent, file))
-
-exit
-ToC=["# title: README", "# description: Here are the files in the repo, with a title and description.","# description: generated from generate_readme.py",""]
+        else:
+          contents+=("\n{}{}{}".format(prefix*(directory_level+1), indent, file))
 
 
-tmatch='^#*\s*title:'
-dmatch='^#*\s*description:'
-
-# print(filelist)
-for name in filelist:
-  # print(name)
-  t=0
-  d=0
-  # open file, read the top parts, get the title: and description: 
-  # then generate markup for it
-  with open(name, "rt") as myfile:
-    for myline in myfile: 
-      if (t!=0):
-        if (d==1):
-          # we are either in the description, or out of it entirely
-          if (re.search(dmatch,myline)):
-            ToC.append("    * " + re.sub(dmatch, "", myline).strip()) 
-          else: # we WERE in the description and now we're not
-            ToC.append("")
-            break
-        else: # we haven't seen a description yet, but we have seen the title
-            if (re.search(dmatch,myline)):
-              d=1
-              ToC.append("    * " + re.sub(dmatch, "", myline).strip()) 
-      if (re.search(tmatch,myline)):
-        t=1
-        ToC.append("* " + name[2:].strip() + " - " + re.sub(tmatch, "", myline.strip()))
+ToC=["# LeetCode Solutions", "# description: Here are the files in the repo, with a title and description.","# description: generated from generate_readme.py",""]
 
 with open("README.md", "w") as outfile:
-  for i in ToC:
-    outfile.write(i + "\n")
+  outfile.write(contents)
+#  for i in ToC:
+#    outfile.write(i + "\n")
